5f834644ae2a133a37896be7f72b121b9a050069
allanac <acole006@fiu.edu>
2017-09-21 16:53:26 -0400

    fixed title

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // default value for title local
-app.locals.title = 'Express - (FIT BUDDY APP TEST)';
+app.locals.title = 'Fit Buddy App';
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
